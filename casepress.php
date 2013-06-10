a8ab3070a82dc3b7729f96d65c6453fd7f882bca
casepress <box.case.press@gmail.com>
2013-06-10 07:46:01 +0600

    rem empty lines

--- casepress.php
+++ casepress.php
@@ -11,21 +11,16 @@
 	 
 	include_once 'cp-core/core-includes.php';
 	include_once 'cp-includes/load.php';
-	
 	include_once 'cp-components/components-includes.php';
 	include_once 'cp-templates/template-include.php';
 
-
 	register_activation_hook( __FILE__, 'cp_activation' );
-
 	function cp_activation() {
 		do_action( 'cp_activate', $func );
 	}
 
 	register_deactivation_hook( __FILE__, 'cp_deactivation' );
-
 	function cp_deactivation() {
 		do_action( 'cp_deactivate', $func );
 	}
-
 ?>
