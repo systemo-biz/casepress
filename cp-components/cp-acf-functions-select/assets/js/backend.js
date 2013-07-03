b525eca489d7e895e7f95383ded8aae2b9e73255
Дмитрий <trasyhoob@gmail.com>
2013-07-03 16:17:06 +0600

    acf integration

--- cp-components/cp-acf-functions-select/assets/js/backend.js
+++ cp-components/cp-acf-functions-select/assets/js/backend.js
@@ -1,4 +1,19 @@
 (function($){
+	
+	$(document).ready(function(){
+		// vars
+		var values = [];
+		
+		$('select#cp_case_category_select').find(':selected').each(function(){
+			values.push( $(this).val() );
+		});
+		
+		acf.screen.post_category = values;
+		acf.screen.taxonomy = values;
+
+		$(document).trigger('acf/update_field_groups');
+		
+	});
 
 	$('select#cp_case_category_select').live('change', function(){
 		// vars
