0463fb1cae3812db76222dfb5e75b82e8a73a2f6
Дмитрий <trasyhoob@gmail.com>
2013-07-03 14:23:48 +0600

    acf integration update

--- cp-components/cp-acf-functions-select/inc/component/assets.php
+++ cp-components/cp-acf-functions-select/inc/component/assets.php
@@ -3,7 +3,6 @@
 	global $pagenow;
 
 	if ( is_admin() && in_array( $pagenow, array( 'post.php', 'post-new.php' ) ) ) {
-		wp_register_script( $this->slug . '-backend', $this->url . '/assets/js/backend.js', array( 'jquery' ), $this->class_version, true );
-		wp_enqueue_script( $this->slug . '-backend' );
+		wp_enqueue_script( $this->slug . '-backend', $this->url . '/assets/js/backend.js', array( 'jquery' ), $this->class_version);
 	}
 ?>
