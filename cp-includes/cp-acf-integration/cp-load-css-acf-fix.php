42a35c3c6e85a09a29ae496af87fb198e3de28c4
casepress <box.case.press@gmail.com>
2013-07-29 22:53:54 +0600

    deregister admin script ACF form site

--- cp-includes/cp-acf-integration/cp-load-css-acf-fix.php
+++ cp-includes/cp-acf-integration/cp-load-css-acf-fix.php
@@ -5,5 +5,13 @@ function cp_load_css_acf_fix_ss(){
 	$path_to_plugin = trailingslashit(plugin_dir_url(__FILE__) );
     
 	wp_enqueue_style( 'acf_fix', $path_to_plugin.'assets/css/acf_fix.css', false, false, 'all' );
-	}
+}
+
+
+// deregister admin style http://www.advancedcustomfields.com/resources/tutorials/creating-a-front-end-form/
+add_action( 'wp_print_styles', 'my_deregister_styles', 100 );
+function my_deregister_styles() {
+	wp_deregister_style( 'wp-admin' );
+}
+
 ?>
