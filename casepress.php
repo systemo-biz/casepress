f59426055b983fd139c78ce5126737b3a4189fda
casepress <box.case.press@gmail.com>
2013-07-31 00:43:02 +0600

    chg version

--- casepress.php
+++ casepress.php
@@ -6,7 +6,7 @@
   Description: Adaptive Case Managment System based on WordPress
   Author: CasePress
   Author URI: http://casepress.org
-  Version: 0.5beta20130715-05
+  Version: 0.5beta20130730-01
 */
 	//include_once 'cp-includes/restrict-access.php';
 	include_once 'cp-includes/load.php';
