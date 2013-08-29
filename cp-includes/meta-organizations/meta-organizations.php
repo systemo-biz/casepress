130603de7228bcb4994b650232e0f5296f4b1db0
casepress <box.case.press@gmail.com>
2013-08-29 08:38:34 +0600

    add taxonomy for processes

--- cp-includes/meta-organizations/meta-organizations.php
+++ cp-includes/meta-organizations/meta-organizations.php
@@ -9,23 +9,23 @@ Description: Description business processes and org unit
 function model_process() {
 
 	$labels = array(
-		'name'                => _x( 'Processes', 'Post Type General Name', 'meta_organization' ),
-		'singular_name'       => _x( 'Process', 'Post Type Singular Name', 'meta_organization' ),
-		'menu_name'           => __( 'Processes', 'meta_organization' ),
-		'parent_item_colon'   => __( 'Parent', 'meta_organization' ),
-		'all_items'           => __( 'All', 'meta_organization' ),
-		'view_item'           => __( 'View', 'meta_organization' ),
-		'add_new_item'        => __( 'Add New', 'meta_organization' ),
-		'add_new'             => __( 'New', 'meta_organization' ),
-		'edit_item'           => __( 'Edit', 'meta_organization' ),
-		'update_item'         => __( 'Update', 'meta_organization' ),
-		'search_items'        => __( 'Search', 'meta_organization' ),
-		'not_found'           => __( 'Not found', 'meta_organization' ),
-		'not_found_in_trash'  => __( 'Not found in Trash', 'meta_organization' ),
+		'name'                => _x( 'Processes', 'Post Type General Name', 'casepress' ),
+		'singular_name'       => _x( 'Process', 'Post Type Singular Name', 'casepress' ),
+		'menu_name'           => __( 'Processes', 'casepress' ),
+		'parent_item_colon'   => __( 'Parent', 'casepress' ),
+		'all_items'           => __( 'All', 'casepress' ),
+		'view_item'           => __( 'View', 'casepress' ),
+		'add_new_item'        => __( 'Add New', 'casepress' ),
+		'add_new'             => __( 'New', 'casepress' ),
+		'edit_item'           => __( 'Edit', 'casepress' ),
+		'update_item'         => __( 'Update', 'casepress' ),
+		'search_items'        => __( 'Search', 'casepress' ),
+		'not_found'           => __( 'Not found', 'casepress' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'casepress' ),
 	);
 	$args = array(
-		'label'               => __( 'Processes', 'meta_organization' ),
-		'description'         => __( 'Desciption processes', 'meta_organization' ),
+		'label'               => __( 'Processes', 'casepress' ),
+		'description'         => __( 'Desciption processes', 'casepress' ),
 		'labels'              => $labels,
 		'supports'            => array( 'title', 'editor', 'comments', 'page-attributes', ),
 		'hierarchical'        => true,
@@ -53,23 +53,23 @@ add_action( 'init', 'model_process', 0 );
 function model_orgunits() {
 
 	$labels = array(
-		'name'                => _x( 'Org Items', 'Post Type General Name', 'meta_organization' ),
-		'singular_name'       => _x( 'Org Item', 'Post Type Singular Name', 'meta_organization' ),
-		'menu_name'           => __( 'Org Items', 'meta_organization' ),
-		'parent_item_colon'   => __( 'Parent', 'meta_organization' ),
-		'all_items'           => __( 'All', 'meta_organization' ),
-		'view_item'           => __( 'View', 'meta_organization' ),
-		'add_new_item'        => __( 'Add New', 'meta_organization' ),
-		'add_new'             => __( 'New', 'meta_organization' ),
-		'edit_item'           => __( 'Edit', 'meta_organization' ),
-		'update_item'         => __( 'Update', 'meta_organization' ),
-		'search_items'        => __( 'Search', 'meta_organization' ),
-		'not_found'           => __( 'Not found', 'meta_organization' ),
-		'not_found_in_trash'  => __( 'Not found in Trash', 'meta_organization' ),
+		'name'                => _x( 'Org Items', 'Post Type General Name', 'casepress' ),
+		'singular_name'       => _x( 'Org Item', 'Post Type Singular Name', 'casepress' ),
+		'menu_name'           => __( 'Org Items', 'casepress' ),
+		'parent_item_colon'   => __( 'Parent', 'casepress' ),
+		'all_items'           => __( 'All', 'casepress' ),
+		'view_item'           => __( 'View', 'casepress' ),
+		'add_new_item'        => __( 'Add New', 'casepress' ),
+		'add_new'             => __( 'New', 'casepress' ),
+		'edit_item'           => __( 'Edit', 'casepress' ),
+		'update_item'         => __( 'Update', 'casepress' ),
+		'search_items'        => __( 'Search', 'casepress' ),
+		'not_found'           => __( 'Not found', 'casepress' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'casepress' ),
 	);
 	$args = array(
-		'label'               => __( 'Org Item', 'meta_organization' ),
-		'description'         => __( 'Desciption Organization', 'meta_organization' ),
+		'label'               => __( 'Org Item', 'casepress' ),
+		'description'         => __( 'Desciption Organization', 'casepress' ),
 		'labels'              => $labels,
 		'supports'            => array( 'title', 'editor', 'comments', 'page-attributes', ),
 		'hierarchical'        => true,
@@ -91,4 +91,41 @@ function model_orgunits() {
 
 // Hook into the 'init' action
 add_action( 'init', 'model_orgunits', 0 );
+
+
+// Register Custom Taxonomy
+function model_process_category()  {
+
+	$labels = array(
+		'name'                       => _x( 'Category Process', 'Taxonomy General Name', 'casepress' ),
+		'singular_name'              => _x( 'Category Process', 'Taxonomy Singular Name', 'casepress' ),
+		'menu_name'                  => __( 'Category Process', 'casepress' ),
+		'all_items'                  => __( 'All', 'casepress' ),
+		'parent_item'                => __( 'Parent', 'casepress' ),
+		'parent_item_colon'          => __( 'Parent:', 'casepress' ),
+		'new_item_name'              => __( 'New', 'casepress' ),
+		'add_new_item'               => __( 'Add New', 'casepress' ),
+		'edit_item'                  => __( 'Edit', 'casepress' ),
+		'update_item'                => __( 'Update', 'casepress' ),
+		'separate_items_with_commas' => __( 'Separate with commas', 'casepress' ),
+		'search_items'               => __( 'Search', 'casepress' ),
+		'add_or_remove_items'        => __( 'Add or remove', 'casepress' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'casepress' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'process_category', 'process', $args );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'model_process_category', 0 );
+
 ?>
