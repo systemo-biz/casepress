072b096826879c6c6ea02a2693f21b21cf1f9960
casepress <box.case.press@gmail.com>
2013-11-16 18:44:15 +0600

    b20131016-01

--- casepress.php
+++ casepress.php
@@ -6,7 +6,7 @@
   Description: Adaptive Case Managment System based on WordPress
   Author: CasePress
   Author URI: http://casepress.org
-  Version: b20131005-01
+  Version: b20131016-01
 */
 
 //include_once 'cp-includes/restrict-access.php';
