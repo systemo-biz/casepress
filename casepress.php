d72e9050721e8827f477c9bcf2c626338336f6be
yumashev <a.yumashev@gmail.com>
2014-10-01 14:38:56 +0600

    Update casepress.php

--- casepress.php
+++ casepress.php
@@ -8,7 +8,7 @@
   GitHub Plugin URI: https://github.com/casepress-studio/casepress/
   GitHub Branch: master
   Author URI: http://casepress.org
-  Version: 20131217-03
+  Version: 20141001-01
 */
 
 
