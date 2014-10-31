195e32ad857105d9ae03c5103dbe3f893fcfb9a6
Anatoly Yumashev <a.yumashev@gmail.com>
2014-10-31 21:11:23 +0500

    Update view_sidebars.php

--- cp-includes/view_sidebars.php
+++ cp-includes/view_sidebars.php
@@ -9,11 +9,11 @@ function view_sidebars_cp(){
 
 	if(is_post_type_archive('organizations') or is_singular('organizations')) {
 		dynamic_sidebar( 'organizations' );
-	} elseif (is_post_type_archive('cases') or is_singular('cases')) {
+	} elseif (is_post_type_archive('cases') or is_singular('cases') or is_tax( 'functions' ) {
 		dynamic_sidebar( 'cases' );
-	} elseif (is_post_type_archive('report') or is_singular('report')) {
+	} elseif (is_post_type_archive('report') or is_singular('report') or is_tax( 'report_cat' )) {
 		dynamic_sidebar( 'report' );
-	} elseif (is_post_type_archive('objects') or is_singular('objects')) {
+	} elseif (is_post_type_archive('objects') or is_singular('objects') or is_tax( 'objects_category' )) {
 		dynamic_sidebar( 'objects' );
 	} elseif (is_post_type_archive('persons') or is_singular('persons')) {
 		dynamic_sidebar( 'persons' );
