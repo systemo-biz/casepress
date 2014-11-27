fd6d75b12f1ff510071a07d9d4fbe1932ba9793b
Anatoly Yumashev <a.yumashev@gmail.com>
2014-11-27 23:26:14 +0500

    Update casepress.php

--- casepress.php
+++ casepress.php
@@ -4,7 +4,7 @@
   Plugin URI: http://casepress.org
   Description: Adaptive Case Managment System based on WordPress. Alpha version
   Author: CasePress
-  GitHub Plugin URI: https://github.com/sistemo/casepress/
+  GitHub Plugin URI: https://github.com/systemo-biz/casepress
   GitHub Branch: master
   Author URI: http://casepress.org
   Version: 20141128
