f931225988baf8eb68eb82b54ac8eb0694d568cd
yumashev <a.yumashev@gmail.com>
2015-01-11 11:35:19 +0500

    test comments actions

--- cp-includes/comments_action.php
+++ cp-includes/comments_action.php
@@ -1,12 +1,12 @@
 <?php
 /*
 Plugin Name: commentAction
-Version: 0.1
+Version: 0.7
 Author: dmnbars
 */
 
-function add_comment_fields_place($defaults) {
-    ob_start();
+add_action('comment_form','add_comment_fields_place');
+function add_comment_fields_place() {
     ?>
     <div class="comment-fields">
         <ul class="list-unstyled">
@@ -14,25 +14,11 @@ function add_comment_fields_place($defaults) {
         </ul>
     </div>
     <?php
-    $defaults['comment_notes_after'] .= ob_get_contents();
-    ob_end_clean();
-    return $defaults;
 };
-add_filter('comment_form_defaults','add_comment_fields_place');
 
-function toggle_case() {
-    global $post;
-    if (isset($_POST['closeCase'])) {
-        wp_set_post_tags( $post->ID, $_POST['resultSelect'], true );
-    } elseif (isset($_POST['openCase'])) {
-        wp_remove_object_terms( $post->ID, 'done', 'post_tag');
-    };
-    return;
-};
-add_filter('comment_post', 'toggle_case');
-
+add_action('add_comment_action_cp','add_custom_fields');
 function add_custom_fields() {
-    if(has_tag('done')) {
+    if(has_term('', 'post_tag')) {
         ?>
         <li>
             <input type="checkbox" id="openCase" name="openCase" value="openCase">
@@ -46,22 +32,24 @@ function add_custom_fields() {
             <label for="closeCase">Закрыть дело</label>
         </li>
         <li>
-            <div class="collapse" id="select-result"><span>Укажите результат:</span>
-                <select name="resultSelect">
-                    <option value="done">Готово</option>
-                </select>
+            <div><span>Укажите результат:</span>
+                <?php wp_dropdown_categories('taxonomy=post_tag&hide_empty=0&name=resultSelect&id=resultSelect&hide_if_empty=true'); ?>
             </div>
         </li>
         <?php
     };
 };
-add_action('add_comment_action_cp','add_custom_fields');
 
-function add_another_field() {
-    ?>
-    <li>another_field</li>
-    <?php
+add_action('comment_post', 'toggle_case');
+function toggle_case() {
+    global $post;
+    if (isset($_POST['closeCase'])) {
+        $tag = get_term_by('id', $_POST['resultSelect'], 'post_tag');
+        wp_set_post_terms($post->ID, $tag->slug, 'post_tag');
+    } elseif (isset($_POST['openCase'])) {
+        wp_delete_object_term_relationships( $post->ID, 'post_tag');
+    };
+    return;
 };
-add_action('add_comment_action_cp','add_another_field');
 
 ?>
