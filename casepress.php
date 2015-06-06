d7ff7f9a83cec6a56f2c75bb42ae7373e8010bec
Anatoly Yumashev <yumashev@users.noreply.github.com>
2015-06-06 21:45:35 +0500

    Update casepress.php

--- casepress.php
+++ casepress.php
@@ -7,7 +7,7 @@ Author: CasePress
 Author URI: http://casepress.org
 GitHub Plugin URI: https://github.com/systemo-biz/casepress
 GitHub Branch: master
-Version: 20150530
+Version: 1.20150606
 */
 
 /*
