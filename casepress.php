28a4bad925d0e67d3e28868e14b5bca277119a48
yumashev <a.yumashev@gmail.com>
2015-07-06 19:11:54 +0500

    chg ver

--- casepress.php
+++ casepress.php
@@ -7,7 +7,7 @@ Author: CasePress
 Author URI: http://casepress.org
 GitHub Plugin URI: https://github.com/systemo-biz/casepress
 GitHub Branch: master
-Version: 20150706-2
+Version: 20150706-3
 */
 
 /*
